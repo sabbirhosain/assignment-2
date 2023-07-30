9042877ab537eeb373a417e371c1951e90e4acb5
Sabbir <sabbirhosainbd59@gmail.com>
2023-07-31 01:07:13 +0600

    Complete team section

--- assist/js/app.js
+++ assist/js/app.js
@@ -1,5 +1,9 @@
-const header = document.querySelector('.header');
 
+/*==============================
+    This is Nav Sticky
+===============================*/
+
+const header = document.querySelector('.header');
 window.onscroll = function () {
   if (document.documentElement.scrollTop >= 20) {
     header.classList.add('nav-sticky');
@@ -7,3 +11,24 @@ window.onscroll = function () {
     header.classList.remove('nav-sticky');
   }
 };
+
+
+/*==============================
+    This is Back to Top Button
+===============================*/
+
+// let mybutton = document.getElementById("myBtn");
+// window.onscroll = function () { scrollFunction() };
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
+// // When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// }
